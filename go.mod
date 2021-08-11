a8895e75b0e23a707b753509fee9b99dd0c3c926
Shrish Narayan Asthana <shrishasthana@outlook.com>
2021-08-11 17:22:05 +0530

    add set implementations

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/adonmo/adonmogocommons
 
 go 1.16
 
-require github.com/hashicorp/logutils v1.0.0 // indirect
+require github.com/hashicorp/logutils v1.0.0
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/adonmo/adonmogocommons/env"
 	"github.com/adonmo/adonmogocommons/logger"
+	"github.com/adonmo/adonmogocommons/set"
 )
 
 func main() {
@@ -16,4 +18,26 @@ func main() {
 	logger.InfoF("Printing this message at %v", time.Now())
 	logger.WarnF("Printing this message at %v", time.Now())
 	logger.ErrorF("Printing this message at %v", time.Now())
+
+	sEt := set.NewSet([]interface{}{"hey", "hello"})
+	setInt := set.NewSetInt([]int{1, 2, 3, 4, 4})
+	setString := set.NewSetString([]string{"hello", "world", "world"})
+
+	logger.DebugF("length of set with interfaces %v", sEt.Len())
+	logger.DebugF("length of set with ints %v", setInt.Len())
+	logger.DebugF("length of set with strings %v", setString.Len())
+
+	logger.DebugF("values of set with ints before mutation %v", setInt.Values())
+	logger.DebugF("values of set with string before mutation %v", setString.Values())
+
+	setInt.Map(func(i int) int {
+		return i * 2
+	})
+
+	setString.Map(func(s string) string {
+		return fmt.Sprintf("%s_mutated", s)
+	})
+
+	logger.DebugF("values of set with ints after mutation %v", setInt.Values())
+	logger.DebugF("values of set with string after mutation %v", setString.Values())
 }
